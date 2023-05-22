a2306acd2ab44c324c02613affe11312bb912791
seonwoo <suika1232@gmail.com>
2023-05-22 17:31:20 +0900

    Sw컨트롤러 매핑 수정

--- HumanGroupware/src/main/java/com/human/springboot/SwController.java
+++ HumanGroupware/src/main/java/com/human/springboot/SwController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SwController {
     
-    @GetMapping("/humangw/board/notice")
+    @GetMapping("/hw/board/notice")
     public String boardNotice(){
         return "board/board_notice";
     }
 
-    @GetMapping("/humangw/board/free")
+    @GetMapping("/hw/board/free")
     public String boardFree(){
         return "board/board_free";
     }
+    
 
 }
