f1b269ddd85e4982527f0ed9a21520eb778a9ac6
seonwoo <suika1232@gmail.com>
2023-05-23 16:25:05 +0900

    부트스트랩 css,js 파일 업로드

--- HumanGroupware/src/main/java/com/human/springboot/SwController.java
+++ HumanGroupware/src/main/java/com/human/springboot/SwController.java
@@ -6,15 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SwController {
     
+    // 테스트용
+    @GetMapping("/temp/main")
+    public String tempMain(){
+        return "temp/temp_layout";
+    }
+
+    // 공지사항
     @GetMapping("/hw/board/notice")
     public String boardNotice(){
         return "board/board_notice";
     }
 
+    // 사내게시판
     @GetMapping("/hw/board/free")
     public String boardFree(){
         return "board/board_free";
     }
+
+    // 게시판 글 작성
+    @GetMapping("/hw/board/insert")
+    public String boardInsert(){
+        return "board/board_insert";
+    }
+
+    
     
 
 }
--- HumanGroupware/src/main/webapp/WEB-INF/views/temp/temp_layout.jsp
+++ HumanGroupware/src/main/webapp/WEB-INF/views/temp/temp_layout.jsp
@@ -6,9 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>Human GroupWare</title>
 </head>
 <body>
     
 </body>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </html>
