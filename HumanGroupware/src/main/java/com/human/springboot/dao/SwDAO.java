ab2d2eb4c9b4313ab57b97519fb872f2eb4c257c
seonwoo <suika1232@gmail.com>
2023-05-31 09:12:44 +0900

    풀 커밋

--- HumanGroupware/src/main/java/com/human/springboot/dao/SwDAO.java
+++ HumanGroupware/src/main/java/com/human/springboot/dao/SwDAO.java
@@ -6,11 +6,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SwDAO {
-    
+   
     ArrayList<SwBoardDTO> boardList(int category);
     void boardInsert(int category, int writer, String title, String content);
     SwBoardDTO boardView(int boardId);
     void boardHit(int board_id);
+    int boardCount(int category);
+
     
 
 }
